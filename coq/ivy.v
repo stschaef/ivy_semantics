5631a191e32567604eda3a6c971133a21e081268
Steven Schaefer <stschaef@umich.edu>
2023-01-23 11:10:58 -0500

    need to workout contexts

--- coq/ivy.v
+++ coq/ivy.v
@@ -1,15 +1,30 @@
 (* Add LoadPath "/home/stschaef/ivy_semantics/coq". *)
 Add LoadPath "/Users/stevenschaefer/ivy_semantics/coq" as coq.
 (* From coq Require Import Maps. *)
-Require Import List.
+Require Import Coq.Lists.List Coq.Bool.Bool.
+Import Coq.Lists.List.ListNotations.
 Require Import String.
 Open Scope string_scope.
 From coq Require Import Maps.
 
+Scheme Equality for list.
+
 Inductive Ivytype : Type :=
   | Ivytype_Bool : Ivytype 
   | Ivytype_User_Defined : string -> Ivytype
-  | Ivytype_Fun : list Ivytype -> Ivytype -> Ivytype.
+  | Ivytype_Fun : list Ivytype -> Ivytype -> Ivytype
+  | Ivytype_Void.
+
+Print list_beq.
+
+Fixpoint eqb_Ivytype (t1 t2 : Ivytype) : bool :=
+  match t1, t2 with
+  | Ivytype_Bool, Ivytype_Bool => true
+  | Ivytype_User_Defined s1, Ivytype_User_Defined s2 => eqb s1 s2
+  | Ivytype_Fun ts1 t1, Ivytype_Fun ts2 t2 => andb (list_beq Ivytype eqb_Ivytype ts1 ts2) (eqb_Ivytype t1 t2)
+  | Ivytype_Void, Ivytype_Void => true
+  | _, _ => false
+  end.
 
 Inductive Expr : Type :=
   | Expr_VarLiteral : string -> Expr
@@ -51,10 +66,8 @@ Inductive Value_Expr : Expr -> Prop :=
   | Value_Expr_False : Value_Expr Expr_False
   | Value_Expr_Var_Fun : forall x es, (forall e, In e es -> Value_Expr e) -> Value_Expr (Expr_VarFun x es).
 
-Definition Context_Var := partial_map (Ivytype * Expr).
-Definition Context_Var_Fun := partial_map (Ivytype * (list Expr -> Expr)).
-Definition Context_Action := partial_map (Ivytype * (list Expr -> Com)).
-Definition Context_Type := partial_map nat.
+Definition value (e : Expr) : Prop := Value_Expr e.
+  
 
 Fixpoint subst (e : Expr) (v : Expr) (x : string) : Expr :=
   match e with 
@@ -74,24 +87,27 @@ Fixpoint subst (e : Expr) (v : Expr) (x : string) : Expr :=
   | Expr_Nondet t => Expr_Nondet t
   | Expr_Cond e1 e2 e3 => Expr_Cond (subst e1 v x) (subst e2 v x) (subst e3 v x)
   end.
-
-Definition unpack_id_under_expr (e : Expr) : option string :=
-  match e with
-  | Expr_VarLiteral x => Some x
-  | Expr_VarFun x es => Some x
-  | Expr_ActionApplication x es => Some x
-  | _ => None (* Shouldn't be able to extract id from anything else *)
-  end.
-
-Print option.
-
-Definition fromMaybe {A : Type} (x : A) (y : option A) : A :=
-  match y with
-  | Some z => z
-  | None => x
-  end.
-
-
+  
+  
+  Definition unpack_id_under_expr (e : Expr) : option string :=
+    match e with
+    | Expr_VarLiteral x => Some x
+    | Expr_VarFun x es => Some x
+    | Expr_ActionApplication x es => Some x
+    | _ => None (* Shouldn't be able to extract id from anything else *)
+    end.
+    
+  Definition context := partial_map Ivytype.
+  Definition store (A : Type) := partial_map A.
+  Definition store_lookup {A} (s : store A) (x : string) : option A := s x.
+  Definition store_update {A} (s : store A) (x : string) (v : A) : store A := t_update s x (Some v).
+  Definition EnumTypeSizes := partial_map nat.
+  
+  Definition fromMaybe {A : Type} (x : A) (y : option A) : A :=
+    match y with
+    | Some z => z
+    | None => x
+    end.
 
 Fixpoint subst_com (p : Com) (v : Expr) (x : string) : Com :=
   match p with 
@@ -107,28 +123,108 @@ Fixpoint subst_com (p : Com) (v : Expr) (x : string) : Com :=
   | _ => p (* declarations *)
   end.
 
-Fixpoint type_expr 
-(e : Expr) (var_ctx : Context_Var) (fun_var_ctx : Context_Var_Fun) 
-(act_ctx : Context_Action) (type_ctx : Context_Type) : option Ivytype :=
+  
+  Fixpoint type_expr 
+  (e : Expr) (var_ctx : context) (fun_var_ctx : context) 
+  (act_ctx : context) (type_ctx : context) (type_sizes : EnumTypeSizes) {struct e} : option Ivytype := 
   match e with
-  | Expr_VarLiteral x => match var_ctx (x) with
-                         | Some (t, e') => Some t
-                         | None => None
-                         end
-  | Expr_VarFun f es => match fun_var_ctx (f) with
-                        | Some (t, e') => (
-                            let arg_types := map (fun e => type_expr e var_ctx fun_var_ctx act_ctx type_ctx) es in
-                            match t with
-                            | Ivytype_Fun arg_types' ret_type => 
-                              if (andb (eqb (length arg_types) (length arg_types')) (andb (forallb (fun x => match x with | Some _ => true | None => false end) arg_types) (forallb (fun x => match x with | Some _ => true | None => false end) arg_types'))) then
-                                Some ret_type
-                              else
-                                None
-                            | _ => None
-                            end
-                        )
-                        | None => None
-                        end
+  | Expr_VarLiteral x => var_ctx x
+  | Expr_VarFun x es => 
+    match fun_var_ctx x with
+      | Some (Ivytype_Fun t1 t2) => 
+        match type_exprs es var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+        | Some ts => 
+        if (list_beq Ivytype eqb_Ivytype ts t1) then Some t2 else None
+        | None => None
+        end
+      | _ => None
+    end
+  | Expr_ActionApplication x es =>
+  match act_ctx x with
+  | Some (Ivytype_Fun t1 t2) => 
+  match type_exprs es var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some ts => 
+  if (list_beq Ivytype eqb_Ivytype ts t1) then Some t2 else None
+  | None => None
+  end
   | _ => None
+  end
+  | Expr_True => Some Ivytype_Bool
+  | Expr_False => Some Ivytype_Bool
+  | Expr_Not e => 
+  match type_expr e var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | Expr_And e1 e2 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | _ => None
+  end
+  | Expr_Or e1 e2 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | _ => None
+  end
+  | Expr_Eq e1 e2 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some t1 => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some t2 => 
+  if (eqb_Ivytype t1 t2) then Some Ivytype_Bool else None
+  | _ => None
+  end
+  | _ => None
+  end
+  | Expr_Implies e1 e2 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | _ => None
+  end
+  | Expr_Iff e1 e2 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | _ => None
+  end
+  | Expr_Forall x t e =>
+  match type_expr e (store_update var_ctx x t) fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | Expr_Exists x t e =>
+  match type_expr e (store_update var_ctx x t) fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => Some Ivytype_Bool
+  | _ => None
+  end
+  | Expr_Nondet t => Some t
+  | Expr_Cond e1 e2 e3 =>
+  match type_expr e1 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some Ivytype_Bool => 
+  match type_expr e2 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some t2 => 
+  match type_expr e3 var_ctx fun_var_ctx act_ctx type_ctx type_sizes with
+  | Some t3 => 
+  if (eqb_Ivytype t2 t3) then Some t2 else None
+  | _ => None
+  end
+  | _ => None
+  end
+  | _ => None
+  end
   end.
-
