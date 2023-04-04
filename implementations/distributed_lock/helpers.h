b77ae2339010a9f3dc25ffb717e853e2970d2af3
elanortang <elanor@umich.edu>
2023-04-04 14:58:45 -0400

    distributed lock

---
+++ implementations/distributed_lock/helpers.h
@@ -0,0 +1,143 @@
+#include <arpa/inet.h>		// htons(), ntohs()
+#include <netdb.h>		// gethostbyname(), struct hostent
+#include <netinet/in.h>		// struct sockaddr_in
+#include <stdio.h>		// perror(), fprintf()
+#include <string.h>		// memcpy()
+#include <sys/socket.h>		// getsockname()
+#include <unistd.h>		// stderr
+
+static const int MAX_MESSAGE_SIZE = 256;
+
+/**
+ * Make a server sockaddr given a port.
+ * Parameters:
+ *		addr: 	The sockaddr to modify (this is a C-style function).
+ *		port: 	The port on which to listen for incoming connections.
+ * Returns:
+ *		0 on success, -1 on failure.
+ * Example:
+ *		struct sockaddr_in server;
+ *		int err = make_server_sockaddr(&server, 8888);
+ */
+int make_server_sockaddr(struct sockaddr_in *addr, int port) {
+	// Step (1): specify socket family.
+	// This is an internet socket.
+	addr->sin_family = AF_INET;
+
+	// Step (2): specify socket address (hostname).
+	// The socket will be a server, so it will only be listening.
+	// Let the OS map it to the correct address.
+	addr->sin_addr.s_addr = INADDR_ANY;
+
+	// Step (3): Set the port value.
+	// If port is 0, the OS will choose the port for us.
+	// Use htons to convert from local byte order to network byte order.
+	addr->sin_port = htons(port);
+
+	return 0;
+}
+
+/**
+ * Make a client sockaddr given a remote hostname and port.
+ * Parameters:
+ *		addr: 		The sockaddr to modify (this is a C-style function).
+ *		hostname: 	The hostname of the remote host to connect to.
+ *		port: 		The port to use to connect to the remote hostname.
+ * Returns:
+ *		0 on success, -1 on failure.
+ * Example:
+ *		struct sockaddr_in client;
+ *		int err = make_client_sockaddr(&client, "141.88.27.42", 8888);
+ */
+int make_client_sockaddr(struct sockaddr_in *addr, const char *hostname, int port) {
+	// Step (1): specify socket family.
+	// This is an internet socket.
+	addr->sin_family = AF_INET;
+
+	// Step (2): specify socket address (hostname).
+	// The socket will be a client, so call this unix helper function
+	// to convert a hostname string to a useable `hostent` struct.
+	struct hostent *host = gethostbyname(hostname);
+	if (host == nullptr) {
+		fprintf(stderr, "%s: unknown host\n", hostname);
+		return -1;
+	}
+	memcpy(&(addr->sin_addr), host->h_addr, host->h_length);
+
+	// Step (3): Set the port value.
+	// Use htons to convert from local byte order to network byte order.
+	addr->sin_port = htons(port);
+
+	return 0;
+}
+
+/**
+ * Return the port number assigned to a socket.
+ *
+ * Parameters:
+ * 		sockfd:	File descriptor of a socket
+ *
+ * Returns:
+ *		The port number of the socket, or -1 on failure.
+ */
+ int get_port_number(int sockfd) {
+ 	struct sockaddr_in addr;
+	socklen_t length = sizeof(addr);
+	if (getsockname(sockfd, (sockaddr *) &addr, &length) == -1) {
+		perror("Error getting port of socket");
+		return -1;
+	}
+	// Use ntohs to convert from network byte order to host byte order.
+	return ntohs(addr.sin_port);
+ }
+
+
+/**
+ * Sends a string message to the server.
+ *
+ * Parameters:
+ *		hostname: 	Remote hostname of the server.
+ *		port: 		Remote port of the server.
+ * 		message: 	The message to send, as a C-string.
+ * Returns:
+ *		0 on success, -1 on failure.
+ */
+int send_message(const char *hostname, int port, const char *message) {
+    if (strlen(message) > MAX_MESSAGE_SIZE) {
+		perror("Error: Message exceeds maximum length\n");
+		return -1;
+	}
+
+	// (1) Create a socket
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// (2) Create a sockaddr_in to specify remote host and port
+	struct sockaddr_in addr;
+	if (make_client_sockaddr(&addr, hostname, port) == -1) {
+		return -1;
+	}
+
+	// (3) Connect to remote server
+	if (connect(sockfd, (sockaddr *) &addr, sizeof(addr)) == -1) {
+		perror("Error connecting stream socket");
+		return -1;
+	}
+	
+	// (4) Send message to remote server
+	// Call send() enough times to send all the data
+	size_t message_len = strlen(message);
+	size_t sent = 0;
+	do {
+		ssize_t n = send(sockfd, message + sent, message_len - sent, 0);
+		if (n == -1) {
+			perror("Error sending on stream socket");
+			return -1;
+		}
+		sent += n;
+	} while (sent < message_len);
+
+	// (5) Close connection
+	close(sockfd);
+
+	return 0;
+}
---
+++ implementations/distributed_lock/node.cpp
@@ -0,0 +1,209 @@
+#include <arpa/inet.h>		// ntohs()
+#include <stdio.h>		// printf(), perror()
+#include <iostream>
+#include <stdlib.h>		// atoi()
+#include <string.h>		// strlen()
+#include <string>
+#include <strings.h>		// bzero()Uu
+#include <sys/socket.h>		// socket(), connect(), send(), recv()
+#include <unistd.h>		// close()
+#include <thread>
+#include "helpers.h"		// make_client_sockaddr()
+
+int epoch = 0;
+bool held;
+
+std::mutex cout_lock;
+std::mutex state_lock;
+
+int transfer(const char* hostname, int dst_port, int epoch){
+	const char* transfer_str = "transfer ";
+	char epoch_str[10];
+	snprintf(epoch_str, 10, "%d", epoch);
+    char target[256];
+    strcpy(target, transfer_str);
+
+	state_lock.lock();
+	held = false;
+	state_lock.unlock();
+
+    return send_message(hostname, dst_port, strcat(target, epoch_str));
+}
+
+
+/**
+ * Receives a string message from the client and prints it to stdout.
+ *
+ * Parameters:
+ * 		connectionfd: 	File descriptor for a socket connection
+ * 				(e.g. the one returned by accept())
+ * Returns:
+ *		0 on success, -1 on failure.
+ */
+int handle_connection(int connectionfd) {
+
+	// printf("New connection %d\n", connectionfd);
+
+	// (1) Receive message from client.
+
+	char msg[MAX_MESSAGE_SIZE + 1];
+	memset(msg, 0, sizeof(msg));
+
+	// Call recv() enough times to consume all the data the client sends.
+	size_t recvd = 0;
+	ssize_t rval;
+	do {
+		// Receive as many additional bytes as we can in one call to recv()
+		// (while not exceeding MAX_MESSAGE_SIZE bytes in total).
+		rval = recv(connectionfd, msg + recvd, MAX_MESSAGE_SIZE - recvd, 0);
+		if (rval == -1) {
+			perror("Error reading stream message");
+			return -1;
+		}
+		recvd += rval;
+	} while (rval > 0);  // recv() returns 0 when client closes
+
+    char * command = strtok(msg, " ");
+    int msg_epoch = atoi(strtok(NULL, " "));
+
+	// (2) Print out the message
+	cout_lock.lock();
+	printf("Received %s at epoch %d\n", command, msg_epoch);
+	cout_lock.unlock();
+    if (strcmp(command, "transfer") == 0) {
+        if (msg_epoch > epoch) {
+			state_lock.lock();
+			epoch = msg_epoch;
+			held = true;
+			state_lock.unlock();
+			cout_lock.lock();
+            printf("Holds lock at epoch %d\n", msg_epoch);
+			cout_lock.unlock();
+        } else {
+			std::lock_guard<std::mutex> raii(cout_lock);
+            printf("Received epoch %d <= current epoch %d. No lock transfer\n", msg_epoch, epoch);
+        }
+    }
+	// (4) Close connection
+	close(connectionfd);
+
+	return 0;
+}
+
+/**
+ * Endlessly runs a server that listens for connections and serves
+ * them _synchronously_.
+ *
+ * Parameters:
+ *		port: 		The port on which to listen for incoming connections.
+ *		queue_size: 	Size of the listen() queue
+ * Returns:
+ *		-1 on failure, does not return on success.
+ */
+int run_server(int port, int queue_size) {
+
+	// (1) Create socket
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		perror("Error opening stream socket");
+		return -1;
+	}
+
+	// (2) Set the "reuse port" socket option
+	int yesval = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yesval, sizeof(yesval)) == -1) {
+		perror("Error setting socket options");
+		return -1;
+	}
+
+	// (3) Create a sockaddr_in struct for the proper port and bind() to it.
+	struct sockaddr_in addr;
+	if (make_server_sockaddr(&addr, port) == -1) {
+		return -1;
+	}
+
+	// (3b) Bind to the port.
+	if (bind(sockfd, (sockaddr *) &addr, sizeof(addr)) == -1) {
+		perror("Error binding stream socket");
+		return -1;
+	}
+
+	// (3c) Detect which port was chosen.
+	port = get_port_number(sockfd);
+	cout_lock.lock();
+	printf("Server listening on port %d...\n", port);
+	cout_lock.unlock();
+
+	// (4) Begin listening for incoming connections.
+	listen(sockfd, queue_size);
+
+	// (5) Serve incoming connections one by one forever.
+	while (true) {
+		int connectionfd = accept(sockfd, 0, 0);
+		if (connectionfd == -1) {
+			perror("Error accepting connection");
+			return -1;
+		}
+
+		if (handle_connection(connectionfd) == -1) {
+			return -1;
+		}
+	}
+}
+
+// User constraint: need to have node_id = 0 for one node
+int repl(const char* hostname) {
+	while(true){
+		cout_lock.lock();
+		printf("> ");
+		cout_lock.unlock();
+		int port;
+		std::string command;
+		std::cin >> command >> port;
+		int send_epoch = epoch + 1;
+		const char *message = command.c_str();
+	
+		if (strcmp(message, "transfer") != 0) {
+			std::lock_guard<std::mutex> raii(cout_lock);
+			printf("Usage: transfer port_num\n");
+			return 1;
+		}
+		else if (!held){
+			std::lock_guard<std::mutex> raii(cout_lock);
+			printf("Cannot transfer a lock you don't have\n");
+		}
+		else {
+			cout_lock.lock();
+			printf("Transferring to %s:%d at epoch %d\n", hostname, port, send_epoch);
+			cout_lock.unlock();
+			if (transfer(hostname, port, send_epoch) == -1){
+				return 1;
+			}
+		}
+	}
+}
+
+int main(int argc, const char **argv) {
+	// Parse command line arguments
+	if (argc != 4) {
+		std::lock_guard<std::mutex> raii(cout_lock);
+		printf("Usage: ./node port_num hostname node_id\n");
+		return 1;
+	}
+	int port = atoi(argv[1]);
+	const char *hostname = argv[2];
+    int node_id = atoi(argv[3]);
+	held = (node_id == 0);
+
+	std::thread t(run_server, port, 10);
+
+	// if (run_server(port, 10) == -1) {
+	// 	return 1;
+	// }
+
+	return repl(hostname);
+}
+
+// why send a message to yourself?
+// how to always be listening for messages but also always be listening for user input? thread
+// can you open a channel while listening for messages? yes with threads
